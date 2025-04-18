eedd33676082adbd03501f4b29fad87ad1a4a7f6
qqawer <1678627439@qq.com>
2025-04-18 13:55:19 +0800

    调整global

--- global/global.go
+++ global/global.go
@@ -1,6 +1,7 @@
 package global
 
 import (
+	// model "grpctest/model/user"
 	"log"
 	"os"
 	"time"
@@ -36,4 +37,14 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
+	// DB.AutoMigrate(&model.User{})
 }
+	
+	//设置全局的logger,这个logger在我们执行每个sql语句的时候会打印每一行sql
+	//sql才是最重要的，本着这个的原则我尽量的给大家看到每个api背后的sql语句是什么
+
+	//定义一个表结构，将表结构直接生成对应的表 - migrations
+	//迁移schema
+	
+  // Migrate the schema
+  
